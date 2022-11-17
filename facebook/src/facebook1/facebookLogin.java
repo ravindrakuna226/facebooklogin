fada8b1791de9c4314e6b2e5e932ed6cdc379783
ravindrakuna226 <109706136+ravindrakuna226@users.noreply.github.com>
2022-11-17 19:22:14 +0530

    chrome driver path changed

--- facebook/src/facebook1/facebookLogin.java
+++ facebook/src/facebook1/facebookLogin.java
@@ -10,19 +10,19 @@ public class facebookLogin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\deepi\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\RAVINDRA\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		//System.setProperty("webdriver.edge.driver","D:\\Automationfiles\\msedgedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.facebook.com");
-		driver.findElement(By.id("email") ).sendKeys("deepika.kpd@gmail.com");
-		driver.findElement(By.id("pass")).sendKeys("riojan2015");
+		driver.findElement(By.id("email") ).sendKeys("");
+		driver.findElement(By.id("pass")).sendKeys("");
 		driver.findElement(By.name("login")).click();
 		driver.findElement(By.cssSelector("div[aria-label='Account']")).click();
 		driver.findElement(By.xpath("//span[contains(text(),'Log Out')]")).click();
 		driver.findElement(By.xpath("//span[text()='Log Out']")).click();
-		driver.close();
+		//driver.close();
 
 	}
 
